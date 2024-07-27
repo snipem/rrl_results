7da4bd6d9887ce07309399e6036ccc501a00fa4c
Matthias Küch <post@matthias-kuech.de>
2024-07-27 23:08:47 +0200

    Working quick enter

--- enter_standings.sh
+++ enter_standings.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
 
+url="$1"
+result_file_name="results.csv"
 curl -s "$1" > input.html
+
+echo "$1" > $result_file_name
 
 while true 
 do
     cat input.html | 
         pup ".jsParticipant strong text{}" | 
-        fzf --bind=enter:replace-query+print-query >> results.txt || break 
+	sed -e 's/^[[:space:]]*//g' |
+        fzf --bind=enter:replace-query+print-query >> $result_file_name || break
 done
 
